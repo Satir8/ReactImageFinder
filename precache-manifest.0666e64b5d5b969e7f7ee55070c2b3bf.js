4899cd0a279462d62eec09d91e6dd8745e785c9d
Gregory Yakovina <54667797+Satir8@users.noreply.github.com>
2020-02-10 21:56:27 +0300

    Updates

---
+++ precache-manifest.0666e64b5d5b969e7f7ee55070c2b3bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb87bede9486a44c0d408d0c00f1fda",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "32467943ca321736ecfb",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e0f5b063423cc3f9e833",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.02f0e268.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.02f0e268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32467943ca321736ecfb",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.86fb1a74.chunk.js"
+  },
+  {
+    "revision": "9ed00262ab067fcfa080",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.5a6095d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goit-react-hw-03-image-finder/static/media/logo.5d5d9eef.svg"
+  }
+]);
